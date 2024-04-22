140fdf97ef6d901976838962933684fefe4f9611
Miguel Cota <mikeest1972@gmail.com>
2024-04-22 10:15:23 -0500

    Fix: Infinite loop in queue

--- src/crawler.py
+++ src/crawler.py
@@ -3,12 +3,29 @@ import requests
 # from treelib import Node, Tree
 from time import sleep
 import json
+import sys
 
 baseURL = "https://texreg.sos.state.tx.us/public/"
 testURL = baseURL+"readtac$ext.TacPage?sl=R&app=9&p_dir=&p_rloc=&p_tloc=&p_ploc=&pg=1&p_tac=&ti=40&pt=1&ch=46&rl=1"
 startURL = "https://texreg.sos.state.tx.us/public/readtac$ext.ViewTAC?tac_view=5&ti=40&pt=1&ch=46&sch=A&rl=Y"
 regDomain = "texreg.sos.state.tx.us"
 
+
+dotNotGoPaths = ["https://texreg.sos.state.tx.us/public/readtac$ext.ViewTAC?tac_view=3&ti=40&pt=5",
+                 "https://texreg.sos.state.tx.us/public/readtac$ext.ViewTAC?tac_view=3&ti=40&pt=12",
+                 "https://texreg.sos.state.tx.us/public/readtac$ext.ViewTAC?tac_view=3&ti=40&pt=15",
+                 "https://texreg.sos.state.tx.us/public/readtac$ext.ViewTAC?tac_view=3&ti=40&pt=17",
+                 "https://texreg.sos.state.tx.us/public/readtac$ext.ViewTAC?tac_view=3&ti=40&pt=19",
+                 "https://texreg.sos.state.tx.us/public/readtac$ext.ViewTAC?tac_view=3&ti=40&pt=20",
+                 "https://texreg.sos.state.tx.us/public/readtac$ext.ViewTAC?tac_view=3&ti=40&pt=21",
+                 "https://texreg.sos.state.tx.us/public/readtac$ext.ViewTAC?tac_view=3&ti=40&pt=22",
+                 "https://texreg.sos.state.tx.us/public/readtac$ext.viewtac",
+                 "http://www.sos.texas.gov/",
+                 "http://www.sos.texas.gov/texreg/index.shtml",
+                 "http://www.sos.texas.gov/tac/index.shtml",
+                 "http://www.sos.texas.gov/open/index.shtml",
+                 ""
+                 ]
 # tree = Tree()
 linksQueue = []
 # list of visited links
@@ -40,8 +57,14 @@ def getTraversableLinks(linkList:list) -> list:
     traversableLink:list = []
     for link in linkList:
         currentLink = generateURLWithBaseURL(link['href'])
-        if regDomain in currentLink:
-            traversableLink.append(currentLink)
+        try:
+            textInTag = link['name']
+        except:
+            textInTag = ""
+        if (regDomain in currentLink) and (currentLink not in linksQueue):
+            if not((textInTag == "Prev Rule") or (textInTag == "Next Rule")):
+                if(currentLink not in dotNotGoPaths):
+                    traversableLink.append(currentLink)
             #tree.create_node(currentLink,currentLink)
     
     return traversableLink
@@ -54,20 +77,21 @@ def generateURLWithBaseURL(url:str) -> str:
         return baseURL+url
 
 
-def crawl(startURL:str):
-    # get html from start url
-    htmlStr:str = getHtmlFromWeb(startURL)
+def crawl(rootURL:str):
+    # get html from root url
+    htmlStr:str = getHtmlFromWeb(rootURL)
     linksFromCurrentURL = extractLinksFromHTML(htmlStr)
+    visitedList.append(rootURL) # add visited url
     linksNotVisited = []
-    linkDic[startURL] = []
+    linkDic[rootURL] = []
     for link in linksFromCurrentURL:
         
-        # check if the link has been visited
-        if link not in visitedList:
-            linkDic[startURL].append(link)
+        # check if the link has been visited or is a banned link
+        if (link not in visitedList) and (link not in dotNotGoPaths):
+            linkDic[rootURL].append(link)
             linksNotVisited.append(link)
     
-    visitedList.append(startURL)
+    
     return linksNotVisited
 
 def saveTree():
@@ -93,16 +117,61 @@ def saveSnapshot():
     print("Sasving Visited list")
     saveVisitedList()
 
+def getQueueFromFile(fName:str) -> list:
+    # loads the queue from a file name
+    print(f"Loading queue from file {fName}")
+    data = {}
+    newQueue = []
+    with open(fName,'r') as queueFile:
+        data = json.load(queueFile)
+        queueFile.close()
+    try:
+        newQueue = data['queue']
+    except:
+        newQueue = []
+    return newQueue
+
+def getTreeFromFile(fName:str)-> dict:
+    print(f"Loading tree from file {fName}")
+    data = {}
+    
+    with open(fName,'r') as queueFile:
+        data = json.load(queueFile)
+        queueFile.close()
+    return data
+
+def getVisitedFromFile(fName:str) -> list:
+    # loads the visited array from a file name
+    print(f"Loading Visited from file {fName}")
+    data = {}
+    newVisited = []
+    with open(fName,'r') as queueFile:
+        data = json.load(queueFile)
+        queueFile.close()
+    try:
+        newVisited = data['visited']
+    except:
+        newVisited = []
+    return newVisited
+
 
 if __name__ == '__main__':
     print("Wayahead Texas Gov regulation site Assisted living Crawler")
-    visitedList.append(startURL) # addthe initial link
-    linksQueue.append(startURL)
+    if len(sys.argv) > 0:
+        inFile = sys.argv[1]
+        inFile2 = sys.argv[2]
+        inFile3 = sys.argv[3]
+        linksQueue = getQueueFromFile(inFile)
+        linkDic = getTreeFromFile(inFile2)
+        visitedList = getVisitedFromFile(inFile3)
+    else:
+        print("Starting from start URL since no queue file was loaded")
+        linksQueue.append(startURL)
     iterationsToSave = 10
     currentItirations = 0
-    while linksQueue.count != 0:
+    while len(linksQueue) != 0:
         print(f"In queue {len(linksQueue)}, Visited: {len(visitedList)}")
-        linksNotVisited = crawl(startURL=linksQueue.pop(-1))
+        linksNotVisited = crawl(rootURL=linksQueue.pop(-1))
         linksQueue.extend(linksNotVisited) # addes links not visited to the queue
         sleep(1)
         if currentItirations == iterationsToSave:
