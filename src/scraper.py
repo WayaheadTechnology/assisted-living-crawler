3768045e18160aa0d57a334a25614750e02d4b0b
Miguel Cota <mikeest1972@gmail.com>
2024-04-25 14:49:34 -0500

    Added new scraper AI

--- src/scraper.py
+++ src/scraper.py
@@ -5,16 +5,29 @@ from bs4 import BeautifulSoup
 import requests
 from time import sleep
 from tqdm import tqdm
-
+import spacy
 
 treeDic:dict = {}
 indexedDic:dict = {}
+
+nlp = spacy.load('en_core_web_trf')
+noGoodKeyTerms = [
+    "Texas Administrative Code"
+]
+
+
+def testSpacy():
+    text = '''	(a) The DADS commissioner and the DSHS commissioner may appoint a manager or management team to manage and operate a community center in accordance with the Texas Health and Safety Code, §§534.038, 534.039, and 534.040. The DADS commissioner may delegate responsibility for appointing a manager or management team to the DSHS commissioner.
+(b) A community center may request a hearing to appeal the commissioners' decision to appoint a manager or management team in accordance with 1 TAC Chapter 357, Subchapter I (relating to Hearings Under the Administrative Procedure Act). Requesting a hearing stays the appointment unless the commissioners based the appointment on a finding under §534.038(a)(2) or (4) of the Texas Health and Safety Code, which means the commissioners found that the community center or an officer or employee of the center misused state or federal money or endangers or may endanger the life, health, or safety of a person served by the center.'''
+    doc = nlp(text)
+    print(list(doc.ents))
+    pass
 
 def test():
     text = '''	(a) The DADS commissioner and the DSHS commissioner may appoint a manager or management team to manage and operate a community center in accordance with the Texas Health and Safety Code, §§534.038, 534.039, and 534.040. The DADS commissioner may delegate responsibility for appointing a manager or management team to the DSHS commissioner.
 (b) A community center may request a hearing to appeal the commissioners' decision to appoint a manager or management team in accordance with 1 TAC Chapter 357, Subchapter I (relating to Hearings Under the Administrative Procedure Act). Requesting a hearing stays the appointment unless the commissioners based the appointment on a finding under §534.038(a)(2) or (4) of the Texas Health and Safety Code, which means the commissioners found that the community center or an officer or employee of the center misused state or federal money or endangers or may endanger the life, health, or safety of a person served by the center.'''
     language = "en"
-    max_ngram_size = 3
+    max_ngram_size = 2
     deduplication_thresold = 0.9
     deduplication_algo = 'seqm'
     windowSize = 1
@@ -40,19 +53,24 @@ def parseURL(urlStr:str):
     parsedHTML = BeautifulSoup(htmlStr, 'html.parser')
     # finds the html tag that has
     readableText = ""
-    if ".html" in urlStr:
-        pass
-    else:
-        listOfRules = parsedHTML.find_all('ss')
-        for item in listOfRules:
-            readableText += item.getText()
+    readableText = parsedHTML.get_text()
+    # if ".html" in urlStr:
+    #     pass
+    # else:
+    #     listOfRules = parsedHTML.find_all('ss')
+    #     for item in listOfRules:
+    #         readableText += item.getText()
 
-    custom_kw_extractor = yake.KeywordExtractor()
-    keyTerms = custom_kw_extractor.extract_keywords(readableText)
-    indexedDic[urlStr] =  [i[0] for i in keyTerms] # gets first term in tuple
+    # custom_kw_extractor = yake.KeywordExtractor()
+    # keyTerms = custom_kw_extractor.extract_keywords(readableText)
+    # indexedDic[urlStr] =  [i[0] for i in keyTerms] # gets first term in tuple
+    doc = nlp(readableText)
+    indexedDic[urlStr] = []
+    for keyWord in doc.ents:
+        indexedDic[urlStr].append(keyWord.text)
 
 def saveIndexedURLS():
-    with open('indexed.json', 'w') as f:
+    with open('indexed_spacy.json', 'w') as f:
         json.dump(indexedDic, f,ensure_ascii=True,indent=4)
         f.close()
     
@@ -78,7 +96,8 @@ if __name__ == '__main__':
         treeFileName = sys.argv[1]
         loadTreeDic(treeFileName)
     print("Scraper")
-    # print("Test")
+    #print("Test")
+    #testSpacy()
     # test()
     treeDic = loadTreeDic('tree.json')
     indexURLs()
